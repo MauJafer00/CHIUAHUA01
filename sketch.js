873a50e90c69e3e485ef03c44eb40bfb60b166e7
MauJafer00 <142833597+MauJafer00@users.noreply.github.com>
2023-10-27 15:21:51 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var PLAY = 1;
-var END  = 0;
-var gameState = PLAY;
-var score = 0;
+var END = 0;
+var GAMESTATE = PLAY;
+
 var chihuahua;
 var chihuahua_running;
 var ground;
@@ -9,12 +9,11 @@ var groundImage;
 var invisibleGround;
 var obstacleImage1, obstacleImage2, obstacleImage3;
 var obstaclesGroup;
-var obstacle1Image;
-
+var obstacle1Image
 function preload() {
   
   chihuahua_running = loadAnimation("./assets/chihuahua1.png", "./assets/chihuahua2.png", "./assets/chihuahua3.png");
-  groundImage = loadImage("./assets/ground3.png");
+  groundImage = loadImage("./assets/ground2.png");
   obstacleImage1 = loadImage("./assets/obstacleImage1.png");
   obstacleImage2 = loadImage("./assets/obstacleImage2.png");
   obstacleImage3 = loadImage("./assets/obstacleImage3.png");
@@ -24,63 +23,62 @@ function setup() {
 
   createCanvas(600, 200);
   edges = createEdgeSprites();
-  
-
-  ground = createSprite(200,180 , 400, 20);
-  ground.x = ground.width/2;
-  ground.addImage(groundImage);
-  invisibleGround = createSprite(200, 190, 400, 20);
-  invisibleGround.visible = false;
-
   chihuahua = createSprite(50, 160, 20, 50);
-  chihuahua.setCollider('circle',0,0,350)
- // chihuahua.debug = true;
   chihuahua.scale = 0.1;
   chihuahua.addAnimation("running", chihuahua_running);
+
+  ground = createSprite(600,184 , 485, 200);
+  ground.addImage(groundImage);
+  invisibleGround = createSprite(100, 190, 5000, 20);
+  invisibleGround.visible = false; 
 
   obstaclesGroup = new Group();
  
 }
   
 function draw() {
-  // console.log(frameCount);
+  
   background("#80d2e0");
-  // mostrar la puntuacion
-  text("puntuacion"+score,500,50);
-  console.log(gameState)
-  if (gameState==PLAY){
-    score=score+Math.round(frameCount/60);
+
+  if (GAMESTATE === PLAY) {
+    
     ground.velocityX = -2;
-
+    console.log(ground.width);
     if (ground.x < 0) {
-    
-      ground.x = ground.width / 2;
-  
+
+      ground.x = ground.width / 5;
+
     }
+
     if (keyDown("space")) {
-    
+
       chihuahua.velocityY = -10;
-  
+
     }
+
     chihuahua.velocityY = chihuahua.velocityY + 0.5;
-    if(obstaclesGroup.isTouching(chihuahua)){
-      gameState=END;
-     console.log("game estate ",gameState);
-      score=0;
-     }
-     spawnObstacles();
-  }
-  else if(gameState===END){
-    // se detiene el suelo 
-   
-    ground.velocityX = 0;
+    spawnObstacles();
+    if (obstaclesGroup.isTouching(chihuahua)) {
+      
+      GAMESTATE = END; 
+      obstaclesGroup.setLifetimeEach(-5);
+
+    }
 
   }
- //  console.log(ground.width);
+  else if (GAMESTATE === END) {
+    
+    ground.velocityX = 0;
+    chihuahua.velocityY = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    
+  }
   
- 
+  
+  
+  
   chihuahua.collide(invisibleGround);
- 
+
   
  
   drawSprites();
@@ -88,12 +86,13 @@ function draw() {
 }
 function spawnObstacles() {
            
-  if (frameCount % 60 === 0) {
+  if (frameCount % 90 === 3) {
 
-    var obstacle = createSprite(50, 160, 20, 50);
+    var obstacle = createSprite(600, 160, 20, 50);
     obstacle.velocityX = -2 ;
-    obstacle.scale = 0.08;
-    obstacle.lifetime = 210;
+    obstacle.scale = 0.078;
+
+    obstacle.lifetime = 300 ;
     
     var rand = Math.round(random(1, 3));
 
@@ -109,8 +108,10 @@ function spawnObstacles() {
       default: break;
 
     }
+
     obstacle.depth = chihuahua.depth;
-    chihuahua.depth = chihuahua.depth+1;
+    chihuahua.depth = chihuahua.depth + 1;
+
     //añade cada obstaculo al grupo
     obstaclesGroup.add(obstacle);
   }
